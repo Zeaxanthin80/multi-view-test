06c4181a79b9a625529f55f21ff19a550c577cf4
Jose Marquez <jose.marquez012@mymdc.net>
2026-05-31 15:50:36 -0400

    Initial Commit

---
+++ multi-view-test/multi_view_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  multi_view_testApp.swift
+//  multi-view-test
+//
+//  Created by user on 5/31/26.
+//
+
+import SwiftUI
+
+@main
+struct multi_view_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
